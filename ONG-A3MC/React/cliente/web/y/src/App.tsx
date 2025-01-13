4581ba67ce2ee9dd8f3f14d47e6e799e6de986dd
Cayetano Castillo <160121487+CayetanoCastillo@users.noreply.github.com>
2025-01-13 12:32:45 +0100

    asdsadsad
    sadsadsad

---
+++ ONG-A3MC/React/cliente/web/y/src/App.tsx
@@ -0,0 +1,13 @@
+import Header from './componentes/Header'
+import './App.css'
+
+const App = () => {
+  return(
+    <>
+    <Header></Header>
+    <h1>Marketing</h1>
+    </>
+  )
+}
+
+export default App
---
+++ ONG-A3MC/React/cliente/web/y/src/componentes/Header.tsx
@@ -0,0 +1,14 @@
+import './Header.css'
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <img src="./LOGO ASOCIACIÓN CAMBIO COLORES_Mesa de trabajo 1 copia-05.png" alt="" />
+                <h2>ONG AM3C "Creciendo Juntas"</h2>
+            </header>
+        </>
+    );
+}
+
+export default Header;
