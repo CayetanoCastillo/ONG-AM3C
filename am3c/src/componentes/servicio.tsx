3771cadfb400b322471ca8a59693de0054ee9fcb
Cayetano Castillo <160121487+CayetanoCastillo@users.noreply.github.com>
2025-01-21 08:35:53 +0100

    asdsad
    dssadsad

--- am3c/src/componentes/servicio.tsx
+++ am3c/src/componentes/servicio.tsx
@@ -2,27 +2,30 @@ import './servicio.css';
 
 const Servicio = () => {
     return (
-        <div className='servicio'>
-            <h1>Nuestros Servicios</h1>
-            <h2>Contribuyendo con el Bienestar de Nuestra Comunidad</h2>
-            <p>En la <b>Asociación de Mujeres de Tres Cantos,</b>ofrecemos servicios con el fin de
-                aportar bienestar, apoyo y crecimiento a nuestra comunidad. Nuestras terapias y
-                asesorías incluyen:</p>
-            <ul>
-                <li><b>Reiki y Shiatsu:</b>Terapias para el bienestar físico y emocional.</li>
-                <li><b>Atención Psicológica:</b>Espacio seguro para fortalecer la salud mental.</li>
-                <li><b>Abrazo Legal:</b>Apoyo y asesoría jurídica para enfrentar diversas situaciones.</li>
-            </ul>
-            <p>Estos servicios, llevados a cabo por profesionales de confianza, están disponibles
-                mediante cita previa y están abiertos a todas aquellas personas que necesiten de este
-                apoyo en su vida.</p>
-            <div className='grid grid333'>
-                <img src="./foto am3c.jpg" alt="" />
-                <img src="./foto am3c.jpg" alt="" />
-                <img src="./foto am3c.jpg" alt="" />
+        <div>
+                <h1>Nuestros Servicios</h1>
+                <h2>Contribuyendo con el Bienestar de Nuestra Comunidad</h2>
+            <div className=' grid servicio'>
+                <p>En la <b>Asociación de Mujeres de Tres Cantos,</b>ofrecemos servicios con el fin de
+                    aportar bienestar, apoyo y crecimiento a nuestra comunidad. Nuestras terapias y
+                    asesorías incluyen:</p>
+                <ul>
+                    <li><b>Reiki y Shiatsu:</b>Terapias para el bienestar físico y emocional.</li>
+                    <li><b>Atención Psicológica:</b>Espacio seguro para fortalecer la salud mental.</li>
+                    <li><b>Abrazo Legal:</b>Apoyo y asesoría jurídica para enfrentar diversas situaciones.</li>
+                </ul>
+                <p>Estos servicios, llevados a cabo por profesionales de confianza, están disponibles
+                    mediante cita previa y están abiertos a todas aquellas personas que necesiten de este
+                    apoyo en su vida.</p>
+                <div className='grid grid333'>
+                    <img src="./foto am3c.jpg" alt="" />
+                    <img src="./foto am3c.jpg" alt="" />
+                    <img src="./foto am3c.jpg" alt="" />
+                </div>
+                <h2>Agenda tu sesion de apoyo / enlazar con contacto</h2>
             </div>
-            <h2>Agenda tu sesion de apoyo / enlazar con contacto</h2>
         </div>
+
     )
 }
 
