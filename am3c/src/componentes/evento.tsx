75a4052f473e0cf7f551c7870e8ff69e16d858b8
Cayetano Castillo <160121487+CayetanoCastillo@users.noreply.github.com>
2025-01-30 11:14:16 +0100

    asdsda
    asddsa

--- am3c/src/componentes/evento.tsx
+++ am3c/src/componentes/evento.tsx
@@ -7,15 +7,18 @@ const Evento = () => {
                 @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
             </style>
             <div className='grid'>
-            <h1>Eventos Especiales</h1>
-            <h2>Encuentros que transforman</h2>
-            <p>Desde conferencias inspiradoras hasta <b>actividades comunitarias</b> y de
-                empoderamiento, para la <b>Asociación de Mujeres de Tres Cantos</b>, cada evento es una
-                oportunidad para profundizar en nuestros valores de fraternidad y <b>transformación
-                social</b>.</p>
+                <h1>Eventos Especiales</h1>
+                <h2>Encuentros que transforman</h2>
+                <div className='eventoActividades'>
+                    <p>Desde conferencias inspiradoras hasta <b>actividades comunitarias</b> y de
+                        empoderamiento, para la <b>Asociación de Mujeres de Tres Cantos</b>, cada evento es una
+                        oportunidad para profundizar en nuestros valores de fraternidad y <b>transformación
+                            social</b>.
+                    </p>
+                </div>
                 <div className='grid grid50'>
-                <img  className="reseña" src="./reseña.png" alt="" />
-                <img src="./evento.jpeg" alt="" />
+                    <img className="reseña" src="./reseña.png" alt="" />
+                    <img src="./evento.jpeg" alt="" />
                 </div>
             </div>
         </div>
