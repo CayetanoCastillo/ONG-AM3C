6a85fff8129370150c013ece74ee5817f97ff90e
Cayetano Castillo <160121487+CayetanoCastillo@users.noreply.github.com>
2025-02-11 10:46:13 +0100

    sad
    dsa

--- am3c-node/index.js
+++ am3c-node/index.js
@@ -1,10 +1,55 @@
-const express = require('express');
+const express = require ('express');
+const path = require("path");
+const cors = require("cors");
+const puerto = 8080;
 const app = express();
-const puerto = 3000;
-const mongoose = require('mongoose');
+const userSchema = require('./schemas/Socia');
+const mongooseCon = require ('./functions/conexion');
+const mongoose = require("mongoose");
+const Usuario = mongoose.model("Socia", userSchema);
+console.log(userSchema);
+
+
+
 app.get('/', (req, res) => {
     res.sendFile(path.joinb(__dirname, 'index.html'));
 });
 app.listen(puerto, () => {
     console.log('El servidor está corriendo en el puerto ' + puerto);
 });
+
+app.post("/HazteSocia", (req, res) => {
+    console.log(req.body);
+    const nuevoUsuario = new Usuario({
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        apellido2: req.body.apellido2,
+        dni: req.body.dni,
+        domicilo: req.body.domicilio,
+        correo: req.body.correo,
+        telefono: req.body.telefono,
+        
+    })
+    nuevoUsuario.save()
+    .then((usuario) => {
+        console.log("Socia creada correctamente: "+usuario)
+    })
+    res.send("Socia creada correctamente.");
+})
+
+app.post("/contacto", (req, res) => {
+    console.log(req.body);
+    const nuevoUsuario = new Usuario({
+        nombre: req.body.nombre,
+        correo: req.body.correo,
+        telefono: req.body.telefono,
+        asunto: req.body.asunto,
+        mensaje: req.body.mensaje,
+        
+    })
+    nuevoUsuario.save()
+    .then((usuario) => {
+        console.log("Socia creada correctamente: "+usuario)
+    })
+    res.send("Socia creada correctamente.");
+})
---
+++ am3c-node/schemas/Socia.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    nombre:{
+        type: String,
+        required: true
+    },
+    apellido:{
+        type: String,
+        required: true
+    },
+    apellido2:{
+        type: String,
+        required: false
+    },
+    dni:{
+        type: String,
+        required: true
+    },
+    domicilio:{
+        type: String,
+        required: true
+    },
+    correo:{
+        type: String,
+        required: true
+    },
+    telefono: {
+        type: Number,
+        required: true
+    }
+})
+
+module.exports = userSchema;
